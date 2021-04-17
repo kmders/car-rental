db824f6896c35f4fc329761cf481ffe198bead5a
veyselugurkizmaz <veysel@ugurkizmaz.com>
2021-04-17 13:39:20 +0300

    Adding admin area. Adding vehicle brand services and controllers.

---
+++ src/Application/Services/IVehicleBrandService.cs
@@ -0,0 +1,17 @@
+﻿using Domain.DTOs;
+using Domain.Entities;
+using System.Collections.Generic;
+
+namespace Application.Services
+{
+    public interface IVehicleBrandService
+    {
+        void Add(VehicleBrand vehicleBrand);
+        void Update(VehicleBrand vehicleBrand);
+        void Delete(int id);
+        VehicleBrand GetById(int id);
+        List<VehicleBrand> Get(VehicleBrandFilter filter);
+
+        string GetName();
+    }
+}
