0f487f0766b838e42827eb0cacc8bbe051b320d0
veyselugurkizmaz <veysel@ugurkizmaz.com>
2021-04-18 13:38:50 +0300

    Starting to develop vehicle model operations

---
+++ src/Application/Services/Common/BaseService.cs
@@ -0,0 +1,14 @@
+﻿using Application.Infrastructure.Persistence;
+
+namespace Application.Services.Common
+{
+    public abstract class BaseService
+    {
+        private protected ICarRentalDbContext Context { get; }
+
+        public BaseService(ICarRentalDbContext context)
+        {
+            Context = context;
+        }
+    }
+}
