78a7230bbc338a62c7ca95c8cc9ed5e71194a987
veyselugurkizmaz <veysel@ugurkizmaz.com>
2021-04-18 13:38:50 +0300

    Starting to develop vehicle model operations

---
+++ src/WebApp/Areas/Admin/Controllers/VehicleModelController.cs
@@ -0,0 +1,102 @@
+﻿using Application.Services;
+using Domain.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("admin/[controller]/[action]")]
+    public class VehicleModelController : Controller
+    {
+        private IVehicleBrandService VehicleBrandService { get; }
+        private IVehicleModelService VehicleModelService { get; }
+
+        public VehicleModelController(IVehicleBrandService vehicleBrandService, IVehicleModelService vehicleModelService)
+        {
+            VehicleBrandService = vehicleBrandService;
+            VehicleModelService = vehicleModelService;
+        }
+
+        // GET: VehicleModelController
+        public ActionResult Index()
+        {
+            VehicleModelFilter filter = new VehicleModelFilter();
+            var items = VehicleModelService.Get(filter);
+            return View(items);
+        }
+
+        // GET: VehicleModelController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: VehicleModelController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: VehicleModelController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: VehicleModelController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: VehicleModelController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: VehicleModelController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: VehicleModelController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- src/WebApp/Startup.cs
+++ src/WebApp/Startup.cs
@@ -31,6 +31,7 @@ namespace WebApp
             services.AddScoped<ICarRentalDbContext>(provider => provider.GetService<CarRentalDbContext>());
 
             services.AddScoped<IVehicleBrandService, VehicleBrandService>();
+            services.AddScoped<IVehicleModelService, VehicleModelService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
